d37f07423090263f4e51ead214166f85c8b24eda
samu.sar <samuil.sarandev@gmail.com>
2024-03-11 15:40:15 +0200

    Add: added Polls livewire component with basic poll list layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,15 @@
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
   @livewireScripts
 
-  @livewire('create-poll')
+  <div>
+      <h2 class='my-4 text-2xl'>Create Poll</h2>
+      @livewire('create-poll')
+  </div>
+
+  <div>
+      <h2 class='my-4 text-2xl'>Available Polls</h2>
+      @livewire('polls')
+  </div>
 </body>
 
 </html>
